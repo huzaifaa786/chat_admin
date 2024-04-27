a6ba811197030766275afe54130afd79e7f50e55
AB_REHMAN <tritechuzaifa@gmail.com>
2024-04-27 10:55:08 +0500

    Hello

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,20 @@
 <?php
 
-use App\Http\Controllers\Admin\SongController;
-use App\Livewire\ReverbTest;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::view('/', 'layout.admin')->middleware('auth:admin');
-Route::view('/test', 'test');
-Route::get('/linkstorage', function () {
-    $targetFolder = base_path() . '/storage/app/public';
-    $linkFolder = $_SERVER['DOCUMENT_ROOT'] . '/storage';
-    symlink($targetFolder, $linkFolder);
+Route::get('/', function () {
+    return view('welcome');
 });
-Route::view('login', 'admin.auth.login')->name('login');
-Route::group(['namespace' => 'App\Http\Controllers'], function () {
-    //// Admin Routes ///
 
-    Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'as' => 'admin.',], function () {
-        Route::view('login', 'admin.auth.login')->name('login');
-        Route::post('login', 'AuthController@login')->name('adminlogin');
-        Route::get('logout', 'AuthController@logout')->name('logout')->middleware('auth:admin');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-        Route::group(['middleware' => 'auth:admin'], function () {
-            Route::view('dashboard', 'layout.admin')->name('dashboard');
-            Route::resource('song', SongController::class);
-        });
-    });
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';

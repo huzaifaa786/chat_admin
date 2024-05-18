9d17efb919a53be63146a73dacc023940cc1acfd
AB_REHMAN <tritechuzaifa@gmail.com>
2024-05-18 16:46:46 +0500

    hello

--- app/Chatify/CustomChatify.php
+++ app/Chatify/CustomChatify.php
@@ -21,7 +21,7 @@ class CustomChatify extends ChatifyMessenger
      */
     public static function newMessage($data)
     {
-        if (isset($data['type'])) {
+        if ($data['type'] == "INVITE") {
             $message = new Message();
             $message->type = $data['type'];
             $message->room_id = $data['room_id'];
@@ -39,5 +39,54 @@ class CustomChatify extends ChatifyMessenger
             $message->save();
         }
         return $message;
+    }
+
+    /**
+     * Fetch & parse message and return the message card
+     * view as a response.
+     *
+     * @param Message $prefetchedMessage
+     * @param int $id
+     * @return array
+     */
+    public static function parseMessage($prefetchedMessage = null, $id = null)
+    {
+        $msg = null;
+        $attachment = null;
+        $attachment_type = null;
+        $attachment_title = null;
+        if (!!$prefetchedMessage) {
+            $msg = $prefetchedMessage;
+        } else {
+            $msg = Message::where('id', $id)->first();
+            if (!$msg) {
+                return [];
+            }
+        }
+        if (isset($msg->attachment)) {
+            $attachmentOBJ = json_decode($msg->attachment);
+            $attachment = $attachmentOBJ->new_name;
+            $attachment_title = htmlentities(trim($attachmentOBJ->old_name), ENT_QUOTES, 'UTF-8');
+            $ext = pathinfo($attachment, PATHINFO_EXTENSION);
+            // $attachment_type = in_array($ext, $this->getAllowedImages()) ? 'image' : 'file';
+            $attachment_type = 'image';
+        }
+        return [
+            'id' => $msg->id,
+            'from_id' => $msg->from_id,
+            'to_id' => $msg->to_id,
+            'message' => $msg->body,
+            'type' => $msg->type,
+            'room_id' => $msg->room_id,
+            'attachment' => (object) [
+                'file' => $attachment,
+                'title' => $attachment_title,
+                'type' => $attachment_type
+            ],
+            'timeAgo' => $msg->created_at->diffForHumans(),
+            'created_at' => $msg->created_at->toIso8601String(),
+            'isSender' => ($msg->from_id == Auth::user()->id),
+            'seen' => $msg->seen,
+        ];
     }
 }

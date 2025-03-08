d0cef6418745ce4bb824c21230ffaaa88bf10be8
AB_REHMAN <tritechuzaifa@gmail.com>
2025-03-08 11:39:50 +0500

    Add image accessor to Product model for asset URL handling

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Helpers\ImageHelper;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -22,5 +24,13 @@ class Product extends Model
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn(string $value) => asset($value),
+            set: fn(string $value) => ImageHelper::saveImageFromApi($value, 'images/products')
+        );
     }
 }
